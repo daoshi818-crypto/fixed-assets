e26f011d5a39aab99077c4932112201e03b42e7a
daoshi818-crypto <daoshi818@gmail.com>
2026-05-09 17:34:19 +0800

    Update record.js

--- functions/api/record.js
+++ functions/api/record.js
@@ -4,14 +4,14 @@ const APP_SECRET = "KMBW1e83BhwdYRViYb1e1gDPLA2mHXZ8";
 const APP_TOKEN = "NeCRbtLv2a6TfbsD3s1cHq3hncf";
 const TABLE_ID = "tblFGeKrKgVPxfAu";
 
+// 获取 tenant token
 async function getTenantToken() {
-
   const res = await fetch(
     "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
     {
       method: "POST",
       headers: {
-        "Content-Type":"application/json"
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         app_id: APP_ID,
@@ -21,34 +21,28 @@ async function getTenantToken() {
   );
 
   const data = await res.json();
-
   return data.tenant_access_token;
 }
 
 export async function onRequestGet(context) {
-
-  const id = new URL(context.request.url)
-    .searchParams.get("id");
+  const id = new URL(context.request.url).searchParams.get("id");
 
   const token = await getTenantToken();
 
   const res = await fetch(
     `https://open.feishu.cn/open-apis/bitable/v1/apps/${APP_TOKEN}/tables/${TABLE_ID}/records/${id}`,
     {
-      headers:{
-        Authorization:`Bearer ${token}`
+      headers: {
+        Authorization: `Bearer ${token}`
       }
     }
   );
 
   const data = await res.json();
 
-  return new Response(
-    JSON.stringify(data),
-    {
-      headers:{
-        "Content-Type":"application/json"
-      }
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json"
     }
-  );
+  });
 }
